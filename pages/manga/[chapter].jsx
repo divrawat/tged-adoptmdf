67e7994d012789485a22f601cc028c58bdcf65b6
divrawat <divrawat2001@gmail>
2024-07-25 12:59:16 +0530

    sg0sw5eg

--- pages/manga/[chapter].jsx
+++ pages/manga/[chapter].jsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Head from 'next/head';
-import { APP_NAME, DOMAIN, IMAGES_DOMAIN, MANGA_NAME, NEXT_PREVIOUS_PREFIX, IMAGE_PREFIX, CHAPTER_PREFIX, AUTHOR_PAGE, LOGO_URL, chaptersData } from '@/config';
+import { APP_NAME, DOMAIN, IMAGES_DOMAIN, MANGA_SUMMARY, MANGA_NAME, NEXT_PREVIOUS_PREFIX, IMAGE_PREFIX, CHAPTER_PREFIX, AUTHOR_PAGE, LOGO_URL, chaptersData } from '@/config';
 import DisqusComments from '@/components/DisQus';
 export const runtime = 'experimental-edge';
+import React from 'react';
+import dynamic from 'next/dynamic';
+const AdSense = dynamic(() => import('@/components/Adsense'), { ssr: false });
 
 export default function Chapter({ chapterNumber, imageUrls, totalChapters, params, errorcode }) {
 
@@ -80,6 +83,8 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
         window.open('https://oatchaudu.net/4/7457654', '_blank');
     };
 
+
+
     return (
         <>
             {head()}
@@ -92,7 +97,7 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                     <div className="flex justify-between max-w-[800px] mx-auto md:mb-[50px] mt-5">
                         {previousChapter !== null ? (
                             <Link
-                                onClick={handleRedirect}
+                                // onClick={handleRedirect}
                                 href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${previousChapter}`}>
                                 <button className="text-[white] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[black] px-2 py-2 font-semibold">Previous Chapter</button>
                             </Link>
@@ -102,7 +107,7 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
 
                         {nextChapter !== null ? (
                             <Link
-                                onClick={handleRedirect}
+                                // onClick={handleRedirect}
                                 href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${nextChapter}`}>
                                 <button className="text-[white] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[black] px-2 py-2 font-semibold">Next Chapter</button>
                             </Link>
@@ -113,20 +118,54 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                     </div>
                 </div>
 
-                <div className='max-w-[1200px] mx-auto mb-5'>
+                {/* <div className='max-w-[1200px] mx-auto mb-5'>
                     {imageUrls.map((imageUrl, index) => (
                         <div className='allimages' key={index}>
                             <img width={700} height={600} loading="lazy" src={imageUrl} alt={`Chapter ${chapterNumber} Image ${index + 1}`} />
                         </div>
                     ))}
+                </div> */}
+
+
+                <div className="max-w-[1200px] mx-auto mb-5">
+                    {imageUrls.map((imageUrl, index) => (
+                        <React.Fragment key={index}>
+                            <div className="allimages">
+                                <img
+                                    loading="lazy"
+                                    src={imageUrl}
+                                    alt={`Chapter ${chapterNumber} Image ${index + 1}`}
+                                />
+                            </div>
+                            {(index === 0 || index === 2 || index === 4) && (
+                                <AdSense key={`ad-${index}`} />
+                            )}
+                        </React.Fragment>
+                    ))}
                 </div>
 
-                <div className='py-10 bg-[#0f0511]'>
+
+
+
+
+                <div className="bg-[black] relative">
+                    <div className="absolute inset-0 bg-black opacity-80"></div> {/* Dark overlay */}
+                    <div className="pt-10 pb-10 max-w-[1100px] mx-auto px-5 text-[white] relative z-10">
+                        <h2 className="text-center font-extrabold text-3xl">{`More About ${MANGA_NAME} Manga`}</h2>
+                        {MANGA_SUMMARY.map(paragraph => (
+                            <p className="py-7 leading-[2]" key={paragraph.id}>{paragraph.content}</p>
+                        ))}
+                    </div>
+                </div>
+
+
+
+                {/* <div className='py-10 bg-[#0f0511]'>
                     <h2 className='text-4xl text-center text-[white] font-blod px-4 mb-10'>Comment Section</h2>
                     <section className='max-w-[1000px] mx-auto px-5'>
                         <DisqusComments url={`/manga/${URL}`} identifier={chapterNumber} title={`${MANGA_NAME} Chapter ${chapterNumber}`} />
                     </section>
-                </div>
+                </div> */}
             </article>
 
             <Footer />
