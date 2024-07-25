44493b4f1e4c1d785d1558a57b5858f511aaf0cb
divrawat <divrawat2001@gmail>
2024-07-25 14:00:45 +0530

    gfsweg5

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Link from "next/link";
 import { DOMAIN, MANGA_NAME, MANGA_DESCRIPTION, MANGA_AUTHOR, MANGA_RELEASE, MANGA_STATUS, MANGA_ARTIST, MANGA_STUDIO, MANGA_GENRE, APP_DESCRIPTION, APP_NAME, MANGA_SUMMARY, COVER_IMG, AUTHOR_PAGE, LOGO_URL, URL_PREFIX, chaptersData, BEHIND_COVER_IMG, RelatedMangaLinks } from "@/config";
 import Head from "next/head";
+import dynamic from 'next/dynamic';
 const AdSense = dynamic(() => import('@/components/Adsense'), { ssr: false });
 
 export default function Home() {
